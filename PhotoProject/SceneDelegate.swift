8111d58f4c2cb3382a0326dae344526e547d5d51
withseon <dlstjsvv@gmail.com>
2025-01-24 09:09:48 +0900

    Refactor: RootView 변경

--- PhotoProject/SceneDelegate.swift
+++ PhotoProject/SceneDelegate.swift
@@ -18,8 +18,11 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let windowScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: windowScene)
-        let tab = MainTabBarController()
-        window?.rootViewController = tab
+        if let isFirstEnter = UserDefaultsManager.isFirstEnter, isFirstEnter {
+            window?.rootViewController = OnboardingViewController()
+        } else {
+            window?.rootViewController = MainNavigationController(rootViewController: ProfileViewController())
+        }
         window?.makeKeyAndVisible()
     }
 
