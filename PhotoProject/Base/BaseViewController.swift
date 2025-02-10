c849dd8652faa58a0516dfde16a56688e8216168
withseon <dlstjsvv@gmail.com>
2025-02-10 21:24:35 +0900

    Refactor: BaseViewController 초기화 함수 구현

--- PhotoProject/Base/BaseViewController.swift
+++ PhotoProject/Base/BaseViewController.swift
@@ -8,6 +8,10 @@
 import UIKit
 
 class BaseViewController: UIViewController {
+    init() {
+        super.init(nibName: nil, bundle: nil)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         configureView()
@@ -18,4 +22,9 @@ class BaseViewController: UIViewController {
     func configureView() { }
     func configureNavigation() { }
     func configureCollectionView() { }
+    
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 }
